d0d93a9b0ca460469a858da6aae740c8d42e4e52
Mateusz Dominiak <mateusz.dominiak@gmail.com>
2012-10-17 11:29:59 +0200

    update spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Todo do
   it "should create a todo" do
     todo = Todo.create!(name: 'my task')
-    todo.reload.name.should eq 'my task'    
+    todo.reload.name.should eq 'my task'
+    todo.should be_persisted
   end
 end
